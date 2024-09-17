acaa542dad56b0f77f01f9b036a6f70b61e92376
Chang-Ju (David) Kim <cjoak1028@gmail.com>
2024-09-17 19:35:24 -0400

    Created banner component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <div className="font-sans">
       <Header />
+      <Banner />
     </div>
   );
 }
