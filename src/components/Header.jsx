fed646bd41387368b043eac2ba12c1f16cd481f4
Chang-Ju (David) Kim <cjoak1028@gmail.com>
2024-09-17 14:36:20 -0400

    Created header for mobile view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
+import ProfileImg from "../assets/profile.jpg";
+
 const Header = () => {
   return (
-    <div className="px-4 py-5">
-      <div className="">cjkim.dev</div>
+    <div className="px-4 py-5 flex flex-row justify-between items-center">
+      <div className="flex flex-row gap-2 items-center">
+        <img src={ProfileImg} className="w-5 h-5 rounded-full" />
+        <span className="font-bold tracking-widest">cjkim.dev</span>
+      </div>
+      <button className="px-4">Menu</button>
     </div>
   );
 };
