5ef27f3b58a4365b9e96a762296d7e322f21553c
Chang-Ju (David) Kim <cjoak1028@gmail.com>
2024-09-17 15:02:57 -0400

    header is responsive to tablet size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,28 @@ import ProfileImg from "../assets/profile.jpg";
 
 const Header = () => {
   return (
-    <div className="px-4 py-5 flex flex-row justify-between items-center">
-      <div className="flex flex-row gap-2 items-center">
-        <img src={ProfileImg} className="w-5 h-5 rounded-full" />
-        <span className="font-bold tracking-widest">cjkim.dev</span>
+    <div className="px-4 sm:px-6 h-[3.75rem] sm:h-[5.125rem] flex flex-row justify-between items-center">
+      <div className="flex flex-row gap-2 sm:gap-3 items-center">
+        <img src={ProfileImg} className="w-5 h-5 sm:w-7 sm:h-7 rounded-full" />
+        <span className="font-bold tracking-widest sm:text-lg">cjkim.dev</span>
       </div>
-      <button className="px-4">Menu</button>
+      <nav className="hidden sm:block">
+        <ul className="flex flex-row items-center">
+          <li>
+            <a className="px-4">About</a>
+          </li>
+          <li>
+            <a className="px-4">Projects</a>
+          </li>
+          <li>
+            <a className="px-4">Contact</a>
+          </li>
+          <button className="ml-4 px-6 h-12 bg-black text-white rounded-full font-bold">
+            Resume
+          </button>
+        </ul>
+      </nav>
+      <button className="px-4 sm:hidden">Menu</button>
     </div>
   );
 };
