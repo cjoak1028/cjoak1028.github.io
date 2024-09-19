50bdd12edd6957729cb5a5e4725429133e830745
Chang-Ju (David) Kim <cjoak1028@gmail.com>
2024-09-18 22:33:16 -0400

    Refactored footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,48 @@
-import LinkedInLogo from "../assets/logos/linkedin-logo.svg";
-import GithubLogo from "../assets/logos/github-logo.svg";
-import InstagramLogo from "../assets/logos/instagram-logo.svg";
+import LinkedInIcon from "../assets/logos/linkedin-logo.svg";
+import GithubIcon from "../assets/logos/github-logo.svg";
+import InstagramIcon from "../assets/logos/instagram-logo.svg";
+
+const socialLinks = [
+  {
+    social: "LinkedIn",
+    link: "https://www.linkedin.com/in/cj-kim-966351255/",
+    icon: LinkedInIcon,
+  },
+  { social: "Github", link: "https://github.com/cjoak1028", icon: GithubIcon },
+  {
+    social: "Instagram",
+    link: "https://www.instagram.com/chng.juce/",
+    icon: InstagramIcon,
+  },
+];
 
 const Footer = () => {
   return (
     <div className="h-32 md:h-48 flex flex-col gap-5 items-center justify-center">
       <div className="flex flex-row gap-5 md:gap-7">
-        <a
-          href="https://www.linkedin.com/in/cj-kim-966351255/"
-          className="hover:opacity-60 cursor-pointer transition-opacity"
-        >
-          <img src={LinkedInLogo} className="h-6 md:h-7" />
-        </a>
-        <a
-          href="https://github.com/cjoak1028"
-          className="hover:opacity-60 cursor-pointer transition-opacity"
-        >
-          <img src={GithubLogo} className="h-6 md:h-7" />
-        </a>
-        <a
-          href="https://www.instagram.com/chng.juce/"
-          className="hover:opacity-60 cursor-pointer transition-opacity"
-        >
-          <img src={InstagramLogo} className="h-6 md:h-7" />
-        </a>
+        {socialLinks.map((socialLink) => {
+          const { social, link, icon } = socialLink;
+          return (
+            <SocialLink key={social} social={social} link={link} icon={icon} />
+          );
+        })}
       </div>
       <p className="text-black/40">Designed & Built by CJ Kim</p>
     </div>
+  );
+};
+
+const SocialLink = ({ social, link, icon }) => {
+  return (
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label={`Link to CJ's ${social}`}
+      className="hover:opacity-60 cursor-pointer transition-opacity"
+    >
+      <img src={icon} alt={`${social} icon`} className="h-6 md:h-7" />
+    </a>
   );
 };
 
