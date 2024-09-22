6c2c82d5a7be76535c5e8a96b9a628b90b863b48
Chang-Ju (David) Kim <cjoak1028@gmail.com>
2024-09-22 16:38:53 -0400

    Integrated EmailJS into contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,4 +1,40 @@
+import { useState } from "react";
+import emailjs from "@emailjs/browser";
+
 const Contact = ({ propRef }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const serviceId = "service_93nepq2";
+    const templateId = "template_5rsyyjk";
+    const publicKey = "CY3CKwRy31vHWPzCD";
+
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      message: message,
+    };
+
+    try {
+      const response = await emailjs.send(
+        serviceId,
+        templateId,
+        templateParams,
+        publicKey
+      );
+      console.log("Email sent successfully", response);
+      setName("");
+      setEmail("");
+      setMessage("");
+    } catch (error) {
+      console.error("Error sending email", error);
+    }
+  };
+
   return (
     <section
       className="content gap-10 w-full max-w-lg scroll-m-[6.25rem] sm:scroll-m-[8.25rem]"
@@ -10,40 +46,46 @@ const Contact = ({ propRef }) => {
           Get in touch with me via email or social media.
         </h3>
       </div>
-      <form id="contact-form" method="POST">
-        <div className="mb-4">
-          <label htmlFor="name" className="text-xs font-medium">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="fromName" className="text-xs font-medium">
             Name
           </label>
           <input
+            name="fromName"
             type="text"
+            value={name}
             className="w-full p-2 text-sm rounded-md border border-solid border-black/10"
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
-        <div className="mb-4">
-          <label htmlFor="inputEmail" className="text-xs font-medium">
+        <div>
+          <label htmlFor="fromEmail" className="text-xs font-medium">
             Email
           </label>
           <input
+            name="fromEmail"
             type="email"
+            value={email}
             className="w-full p-2 text-sm rounded-md border border-solid border-black/10"
-            aria-describedby="emailHelp"
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
-        <div className="mb-4">
+        <div>
           <label htmlFor="message" className="text-xs font-medium">
             Message
           </label>
           <textarea
+            name="message"
+            value={message}
             className="w-full p-2 text-sm rounded-md border border-solid border-black/10"
             rows="5"
+            onChange={(e) => setMessage(e.target.value)}
           ></textarea>
         </div>
-        <div className="text-end">
-          <button type="submit" className="primary-button">
-            Send message
-          </button>
-        </div>
+        <button type="submit" className="primary-button self-end">
+          Send message
+        </button>
       </form>
     </section>
   );
